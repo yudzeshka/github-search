def6c85c8f9f00d61244b8372a1cde44d01a903e
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-02-15 11:37:17 +0300

    added searchInput, got usersData,  added Tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import "./App.css";
 import UserCard from "./components/UserCard";
 function App() {
   const [users, setUsers] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
   React.useEffect(() => {
     axios
-      .get("https://api.github.com/search/repositories?q=max")
+      .get(`https://api.github.com/search/repositories?q=${searchValue}`)
       .then(({ data }) => {
         setUsers(data.items);
       });
-  }, []);
+  }, [searchValue]);
+  const eventHandler = (event) => setSearchValue(event.target.value);
   console.log(users);
   return (
     <>
-      <input placeholder="search"></input>
+      <input
+        placeholder="search"
+        // value={searchValue}
+        onChange={eventHandler}
+      ></input>
       <div className="usersCards">
         {users &&
           users.map((user) => (
             <UserCard
+              key={user.id}
               avatar={user.owner.avatar_url}
               userName={user.name}
               id={user.id}
