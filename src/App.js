05a0a5311aa997a1aad4d2fc35c0d1d8b157b92b
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-02-18 13:56:27 +0300

    added filter by language

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ function App() {
   const [users, setUsers] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState("");
   const [isSearching, setIsSearching] = React.useState(false);
+  const [languageName, setLanguageName] = React.useState(null);
   const debouncedSearchValue = useDebounce(searchValue, 600);
+
+  function filterBy(data, field, value) {
+    return data.filter((item) => (value ? item[field] === value : item));
+  }
+  const filterByLanguage = filterBy(users, "language", languageName);
+  const eventHandler = (event) => setSearchValue(event.target.value);
 
   React.useEffect(() => {
     if (debouncedSearchValue) {
@@ -24,23 +31,42 @@ function App() {
     return setUsers([]);
   }, [debouncedSearchValue]);
 
-  const eventHandler = (event) => setSearchValue(event.target.value);
   console.log(users);
 
   return (
     <>
-      <input placeholder="search" onChange={eventHandler}></input>
+      <input
+        className="border"
+        placeholder="search"
+        onChange={eventHandler}
+      ></input>
       {isSearching && <div>searching....</div>}
+      <div className="flex justify-around">
+        <button className="border" onClick={() => setLanguageName(null)}>
+          Show All
+        </button>
+        <button
+          className="border"
+          onClick={() => setLanguageName("JavaScript")}
+        >
+          JavaScript
+        </button>
+        <button className="border" onClick={() => setLanguageName("Java")}>
+          Java
+        </button>
+        <button className="border" onClick={() => setLanguageName("Python")}>
+          Python
+        </button>
+      </div>
       <div className="grid grid-cols-4 md:grid-cols-8 grid-flow-row gap-4">
-        {users &&
-          users.map((user) => (
-            <UserCard
-              key={user.id}
-              avatar={user.owner.avatar_url}
-              userName={user.name}
-              language={user.language}
-            />
-          ))}
+        {filterByLanguage.map((user) => (
+          <UserCard
+            key={user.id}
+            avatar={user.owner.avatar_url}
+            userName={user.name}
+            language={user.language}
+          />
+        ))}
       </div>
     </>
   );
