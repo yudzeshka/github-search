9127e981e88c99db201eff82ac101cf1c1e25ee3
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-02-18 14:39:20 +0300

    added react-router, created main and user pages

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function UserCard({ avatar, userName, language }) {
   console.log(userName);
   return (
     <div>
       <div>
-        <img src={avatar}></img>
+        <Link to={"/userpage"}>
+          <img src={avatar}></img>
+        </Link>
       </div>
       <div>name: {userName}</div>
       <div>language: {language}</div>
---
+++ src/pages/Main.js
@@ -0,0 +1,73 @@
+import React from "react";
+import axios from "axios";
+import UserCard from "../components/UserCard";
+import useDebounce from "../services/use-debounce";
+
+export default function Main() {
+  const [users, setUsers] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [isSearching, setIsSearching] = React.useState(false);
+  const [languageName, setLanguageName] = React.useState(null);
+  const debouncedSearchValue = useDebounce(searchValue, 600);
+
+  function filterBy(data, field, value) {
+    return data.filter((item) => (value ? item[field] === value : item));
+  }
+  const filterByLanguage = filterBy(users, "language", languageName);
+  const eventHandler = (event) => setSearchValue(event.target.value);
+
+  React.useEffect(() => {
+    if (debouncedSearchValue) {
+      setIsSearching(true);
+      axios
+        .get(
+          `https://api.github.com/search/repositories?q=${debouncedSearchValue}`
+        )
+        .then(({ data }) => {
+          setIsSearching(false);
+          setUsers(data.items);
+        });
+    }
+    return setUsers([]);
+  }, [debouncedSearchValue]);
+
+  console.log(users);
+
+  return (
+    <>
+      <input
+        className="border"
+        placeholder="search"
+        onChange={eventHandler}
+      ></input>
+      {isSearching && <div>searching....</div>}
+      <div className="flex justify-around">
+        <button className="border" onClick={() => setLanguageName(null)}>
+          Show All
+        </button>
+        <button
+          className="border"
+          onClick={() => setLanguageName("JavaScript")}
+        >
+          JavaScript
+        </button>
+        <button className="border" onClick={() => setLanguageName("Java")}>
+          Java
+        </button>
+        <button className="border" onClick={() => setLanguageName("Python")}>
+          Python
+        </button>
+      </div>
+      <div className="grid grid-cols-4 md:grid-cols-8 grid-flow-row gap-4">
+        {filterByLanguage.map((user) => (
+          <UserCard
+            key={user.id}
+            avatar={user.owner.avatar_url}
+            userName={user.name}
+            language={user.language}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
