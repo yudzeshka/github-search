b3bae31c472d45f48d92059fe1d5b4db002a7cf4
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-02-18 11:14:23 +0300

    added useDebounce hook

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import axios from "axios";
 import React from "react";
 import "./App.css";
 import UserCard from "./components/UserCard";
+import useDebounce from "./services/use-debounce";
 function App() {
   const [users, setUsers] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState("");
+  const [isSearching, setIsSearching] = React.useState(false);
+  const debouncedSearchValue = useDebounce(searchValue, 600);
 
   React.useEffect(() => {
-    axios
-      .get(`https://api.github.com/search/repositories?q=${searchValue}`)
-      .then(({ data }) => {
-        setUsers(data.items);
-      });
-  }, [searchValue]);
+    if (debouncedSearchValue) {
+      setIsSearching(true);
+      axios
+        .get(
+          `https://api.github.com/search/repositories?q=${debouncedSearchValue}`
+        )
+        .then(({ data }) => {
+          setIsSearching(false);
+          setUsers(data.items);
+        });
+    }
+    return setUsers([]);
+  }, [debouncedSearchValue]);
 
   const eventHandler = (event) => setSearchValue(event.target.value);
   console.log(users);
@@ -20,6 +30,7 @@ function App() {
   return (
     <>
       <input placeholder="search" onChange={eventHandler}></input>
+      {isSearching && <div>searching....</div>}
       <div className="grid grid-cols-4 md:grid-cols-8 grid-flow-row gap-4">
         {users &&
           users.map((user) => (
