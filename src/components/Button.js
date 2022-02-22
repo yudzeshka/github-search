68e83afeec36876e0471abde83c52463d0b31aec
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-02-22 09:01:57 +0300

    styles fixed.

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 export default function Button({ buttonName, onClickBtn }) {
   return (
     <button
-      className="border rounded-md mx-5 py-1 px-2 font-bold hover:bg-violet-100"
+      className="border rounded-md  mx-1 mb-2 md:mx-5 py-1 px-2 font-bold hover:bg-violet-100"
       onClick={onClickBtn}
     >
       {buttonName}
